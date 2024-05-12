f2ae63b84ffd1472475be54ad42be7e81a41c909
Gary H <garyy.hd.123@gmail.com>
2024-05-12 20:43:13 +0700

    Add: bouncer package and migration, and fix table name

--- database/migrations/2024_05_12_065438_create_doctors_table.php
+++ database/migrations/2024_05_12_065438_create_doctors_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_doctors', function (Blueprint $table) {
+        Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->string('email')->unique();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Silber\Bouncer\Bouncer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+
+        $roles = [
+            'sa','admin','doctor',
+        ];
+        foreach ($roles as $role) Bouncer::create()->role()->findOrCreateRoles($role);
     }
 }
